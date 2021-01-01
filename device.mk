9b3e8d0031b8b01fb81165e9c5592353a40ad31e
Lugaresi <root@stukamanhpt4.com>
2021-01-01 22:30:42 +0100

    Email has been abandoned.

--- device.mk
+++ device.mk
@@ -37,7 +37,3 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/bcmdhd.cal:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/bcmdhd.cal
 
-# Email not in AICP?!
-PRODUCT_PACKAGES += \
-    Email
-
